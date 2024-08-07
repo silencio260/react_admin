d8d1d21dc94fc20225f4e954a989a974761c4307
silencio260 <faruq5840@gmail.com>
2024-08-07 21:53:51 +0100

    Implement event calendar page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Form from "./scenes/form";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 
 
 
@@ -38,7 +38,7 @@ function App() {
               {/* <Route path="/bar" element={<Bar />}/> */}
               {/* <Route path="/pie" element={<Pie />}/> */}
               {/* <Route path="/line" element={<Line />}/> */}
-              {/* <Route path="/calendar" element={<Calendar />}/> */}
+              <Route path="/calendar" element={<Calendar />}/>
               {/* <Route path="/geography" element={<Geography />}/> */}
               {/* <Route path="/faq" element={<FAQ />}/> */}
             </Routes>
